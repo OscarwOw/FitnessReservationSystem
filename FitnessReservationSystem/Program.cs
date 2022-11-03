33ed982d39785f8cf735afb2511c16ba6cc7519d
OscarwOw <OscarwOw210@gmail.com>
2022-11-04 00:06:44 +0100

    add dbcontext service

--- FitnessReservationSystem/Program.cs
+++ FitnessReservationSystem/Program.cs
@@ -1,7 +1,11 @@
+using FitnessReservationSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DatabaseContext>(option => { option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
