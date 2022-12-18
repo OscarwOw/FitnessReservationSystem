b39c09e9cbf76bfcffdba4bc3343164125e1b452
OscarwOw <OscarwOw210@gmail.com>
2022-12-18 08:57:27 +0100

    aading models

---
+++ FitnessReservationSystem/Dto/UserDtos/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessReservationSystem.Dto.UserDtos
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "User name is required")]
+        public string? Username { get; set; }
+        [Required(ErrorMessage = "User name is required")]
+        public string? Password { get; set; }
+    }
+}
---
+++ FitnessReservationSystem/Dto/UserDtos/RegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessReservationSystem.Dto.UserDtos
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "User name is required")]
+        public string? Username { get; set; }
+        [EmailAddress]
+        [Required(ErrorMessage = "User name is required")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "User name is required")]
+        public string? Password { get; set; }
+    }
+}
